9b3662a71f9eb1ab0def6cb4b6e784eaf7279b7a
Eldar Alimov <alimov_e@auca.kg>
2024-01-30 15:00:59 +0600

    OJ1 Accepted

---
+++ OJ Problems/OJ1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void dfs(TreeNode* root, vector<int> &v){
+        if(root == nullptr) return;
+        dfs(root->left, v);
+        v.push_back(root->val);
+        dfs(root->right, v);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> v;
+        dfs(root, v);
+        return v;
+    }
+};
